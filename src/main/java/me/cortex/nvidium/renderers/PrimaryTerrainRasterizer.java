d2cbece3628d35d8dccc9f07172d0ca7754bf794
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-29 16:12:08 -0300

    Update PrimaryTerrainRasterizer.java

--- src/main/java/me/cortex/nvidium/renderers/PrimaryTerrainRasterizer.java
+++ src/main/java/me/cortex/nvidium/renderers/PrimaryTerrainRasterizer.java
@@ -1,12 +1,14 @@
 package me.cortex.nvidium.renderers;
 
+import android.opengl.GLES20;
+import android.opengl.GLES30;
+
 import com.mojang.blaze3d.platform.GlStateManager;
 import me.cortex.nvidium.gl.shader.Shader;
 import me.cortex.nvidium.sodiumCompat.ShaderLoader;
 import me.cortex.nvidium.mixin.minecraft.LightMapAccessor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
-import org.lwjgl.opengl.GL45;
 import org.lwjgl.opengl.GL45C;
 
 import static me.cortex.nvidium.RenderPipeline.GL_DRAW_INDIRECT_ADDRESS_NV;
@@ -25,40 +27,42 @@ public class PrimaryTerrainRasterizer extends Phase {
             .addSource(FRAGMENT, ShaderLoader.parse(new Identifier("nvidium", "terrain/frag.frag"))).compile();
 
     public PrimaryTerrainRasterizer() {
-        GL45C.glSamplerParameteri(blockSampler,     GL45C.GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-        GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MIN_LOD, 0);
-        GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MAX_LOD, 4);
+        GLES30.glSamplerParameteri(blockSampler, GLES30.GL_TEXTURE_MIN_FILTER, GLES30.GL_NEAREST_MIPMAP_LINEAR);
+        GLES30.glSamplerParameteri(blockSampler, GLES30.GL_TEXTURE_MAG_FILTER, GLES30.GL_NEAREST);
+        GLES30.glSamplerParameteri(blockSampler, GLES30.GL_TEXTURE_MIN_LOD, 0);
+        GLES30.glSamplerParameteri(blockSampler, GLES30.GL_TEXTURE_MAX_LOD, 4);
     }
 
     private static void setTexture(int textureId, int bindingPoint) {
-        GlStateManager._activeTexture(33984 + bindingPoint);
-        GlStateManager._bindTexture(textureId);
+        GLES30.glActiveTexture(33984 + bindingPoint);
+        GLES30.glBindTexture(GLES30.GL_TEXTURE_2D, textureId);
     }
 
     public void raster(int regionCount, long commandAddr) {
         shader.bind();
 
-        int blockId = MinecraftClient.getInstance().getTextureManager().getTexture(new Identifier("minecraft", "textures/atlas/blocks.png")).getGlId();
+        int blockId = loadTexture(new Identifier("minecraft", "textures/atlas/blocks.png"));
         int lightId = ((LightMapAccessor)MinecraftClient.getInstance().gameRenderer.getLightmapTextureManager()).getTexture().getGlId();
 
-        //GL45C.glBindTextureUnit(0, blockId);
-        GL45C.glBindSampler(0, blockSampler);
-
-        //GL45C.glBindTextureUnit(1, lightId);
-        GL45C.glBindSampler(1, lightSampler);
+        GLES30.glBindSampler(0, blockSampler);
+        GLES30.glBindSampler(1, lightSampler);
         setTexture(blockId, 0);
         setTexture(lightId, 1);
 
         glBufferAddressRangeNV(GL_DRAW_INDIRECT_ADDRESS_NV, 0, commandAddr, regionCount*8L);//Bind the command buffer
         glMultiDrawMeshTasksIndirectNV( 0, regionCount, 0);
-        GL45C.glBindSampler(0, 0);
-        GL45C.glBindSampler(1, 0);
+        GLES30.glBindSampler(0, 0);
+        GLES30.glBindSampler(1, 0);
+    }
+
+    private int loadTexture(Identifier textureId) {
+        // TODO: Implement texture loading for Android
+        return 0;
     }
 
     public void delete() {
-        GL45.glDeleteSamplers(blockSampler);
-        GL45.glDeleteSamplers(lightSampler);
+        GLES30.glDeleteSamplers(blockSampler);
+        GLES30.glDeleteSamplers(lightSampler);
         shader.delete();
     }
 }
